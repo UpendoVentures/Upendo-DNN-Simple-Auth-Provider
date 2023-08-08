6029e3c7f3b2330629222a310ba0d013a0612a7e
Ariel Blanco <Ariel.Blanco1990@gmail.com>
2023-08-08 01:19:54 -0400

    Update Email.cs
    Changed the reference to the logo icon displayed in the email so that it gets from a new method that gets it from PortalSettings as you suggested.

--- Modules/UpendoDnnSimpleAuthProvider/Components/Email.cs
+++ Modules/UpendoDnnSimpleAuthProvider/Components/Email.cs
@@ -27,6 +27,7 @@ using System.Linq;
 using System.Web;
 using System.Text;
 using DotNetNuke.Entities.Users;
+using UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components;
 
 namespace UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components
 {
@@ -35,6 +36,7 @@ namespace UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components
         public static void Send(string userEmail, string code)
         {
             var randomCode = code;
+            string logo = UtilityMethods.GetSiteIconUrl().ToString();
             string emailSubject = "Upendo Authenticator Provider confirmation code: " + randomCode;
             var hostEmail = HostController.Instance.GetString("HostEmail");
             var toEmailAddress = userEmail;
@@ -88,7 +90,7 @@ namespace UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components
     <tr>
       <td class=""header"" style=""padding: 24pt 0.75pt"">
         <div align=""center"">
-          <img src=""https://upendo-website.s3-us-west-1.amazonaws.com/Portals/0/Images/Upendo-logo-trans-230x65.png"" alt=""upendo logo"">
+         <img src=""http://"; emailBody += logo; emailBody += $@""" alt=""logo"">
         </div>
       </td>
     </tr>

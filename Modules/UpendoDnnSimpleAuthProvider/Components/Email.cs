4d3afc0f9caf0c6e088401ace134db971537ba2c
Ariel Blanco <Ariel.Blanco1990@gmail.com>
2023-08-09 14:46:58 -0400

    Update Email.cs
    I removed the static HTML content from the email body to get it from the new template file using PortalSettings to get the location of the file that currently contains the email body content.

--- Modules/UpendoDnnSimpleAuthProvider/Components/Email.cs
+++ Modules/UpendoDnnSimpleAuthProvider/Components/Email.cs
@@ -1,4 +1,6 @@
-﻿/*
+﻿#region License
+
+/*
 Copyright © Upendo Ventures, LLC
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
@@ -17,6 +19,8 @@ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#endregion
+
 using DotNetNuke.Data;
 using DotNetNuke.Entities.Controllers;
 using System;
@@ -28,6 +32,9 @@ using System.Web;
 using System.Text;
 using DotNetNuke.Entities.Users;
 using UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components;
+using DotNetNuke.Entities.Portals;
+using System.IO;
+using System.Web.Hosting;
 
 namespace UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components
 {
@@ -37,89 +44,16 @@ namespace UpendoVentures.Auth.UpendoDnnSimpleAuthProvider.Components
         {
             var randomCode = code;
             string logo = UtilityMethods.GetSiteIconUrl().ToString();
-            bool isSecureConnection = HttpContext.Current.Request.IsSecureConnection;
-            string logoUrl = isSecureConnection ? "https://"+ logo : "http://"+ logo;
             string emailSubject = "Upendo Authenticator Provider confirmation code: " + randomCode;
             var hostEmail = HostController.Instance.GetString("HostEmail");
             var toEmailAddress = userEmail;
-            string fecha = DateTime.Now.ToString();
-            string emailBody = @"
-<!DOCTYPE html>
-<html xmlns:v=""urn:schemas-microsoft-com:vml"">
-<head>
-  <meta http-equiv=""Content-Type"" content=""text/html; charset=unicode"">
-  <title>Upendo Authenticator Provider confirmation code:""; emailBody += randomCode; emailBody += $@""</title>
-  <style>
-    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
-    body {
-      font-family: 'Montserrat', Arial, sans-serif; /* Utiliza la fuente Montserrat */
-    }
-    .header h1 {
-      font-weight: 700; 
-    }
-    .heroparagraph,
-    .contentparagraph {
-      font-weight: 300;
-    }
-    .container {
-      width: 100%;
-      background: white;
-      border-collapse: collapse;
-      border-radius: 8px;
-      box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
-    }
-    .header img {
-      width: 120px;
-      height: 36px;
-      margin: 32px 0;
-    }
-    .code-box {
-      background: #F5F4F5;
-      text-align: center;
-      vertical-align: middle;
-      color: black;
-      font-size: 22.5pt;
-    }
-    .footer {
-      text-align: center;
-      padding: 15px;
-      color: dimgray;
-    }
-  </style>
-</head>
-<body>
-  <table class=""container"">
-    <tr>
-      <td class=""header"" style=""padding: 24pt 0.75pt"">
-        <div align=""center"">
-         <img src="""; emailBody += logoUrl; emailBody += $@""" alt=""logo"">
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td style=""padding: 0 0.75pt"">
-        <div style=""margin: 0 37.5pt 22.5pt"">
-          <h1>Confirm your email address</h1>
-          <p class=""heroparagraph"">Your confirmation code is below — enter it in your open browser window and we'll help you get signed in.</p>
-        </div>
-        <div class=""code-box"">"; emailBody += randomCode; emailBody += $@"</div>
-        <div style=""margin: 0 37.5pt 22.5pt"">
-          <p class=""contentparagraph"">If you didn’t request this email, there’s nothing to worry about — you can safely ignore it.</p>
-        </div>
-      </td>
-    </tr>
-    <tr>
-      <td style=""padding: 15pt 0.75pt"">
-        <div class=""footer"">
-          ©2023 Upendo Ventures.<br>
-          548 Market St. #65401, San Francisco, CA 94104<br>All rights reserved.
-        </div>
-      </td>
-    </tr>
-  </table>
-</body>
-</html>";
-            
+            string valueSettings = PortalController.GetPortalSetting("UpendoSimpleDnnAuth.ConfirmEmail", DnnGlobal.Instance.GetPortalId(), string.Empty);
+            string serverPath = HostingEnvironment.MapPath(valueSettings);
+            var templateContent = File.ReadAllText(serverPath);
+            string emailBody = templateContent;
+            emailBody = emailBody.Replace("CODE", randomCode);
+            emailBody = emailBody.Replace("LOGO_URL", logo);
+
             DotNetNuke.Services.Mail.Mail.SendEmail(hostEmail, toEmailAddress, emailSubject, emailBody);
         }
 
